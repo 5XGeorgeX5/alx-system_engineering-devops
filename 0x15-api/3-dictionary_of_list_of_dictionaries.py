f03b15213b9f39d7fb4d23c41db8abbdbb27f43b
George Raafat <georgexraafat@gmail.com>
2023-10-23 13:35:37 -0700

    doing some task

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""returns information about employee's TODO list using a REST API."""
+from requests import get
+import json
+
+if __name__ == "__main__":
+    user = "https://jsonplaceholder.typicode.com/users/"
+    todo = "https://jsonplaceholder.typicode.com/todos?userId="
+    employees = get(user).json()
+    tasks = get(todo).json()
+    data = {str(employee.get('id')): [{
+        "task": task.get('title'),
+        "completed": task.get('completed'),
+        "username": employee.get('username')
+    } for task in get(f"{todo}{employee.get('id')}").json()]
+    for employee in employees}
+    with open("todo_all_employees.json", 'w') as file:
+        json.dump(data, file)
